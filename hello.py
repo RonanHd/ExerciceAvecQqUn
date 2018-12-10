5ed4f9b166ab8cbdaef14a8c7f42e0e703900712
RonanH <hallard.ronan@gmail.com>
2018-12-10 10:50:42 +0100

    Tentons

---
+++ hello.py
@@ -0,0 +1,8 @@
+print("HEY, comment s est passe ton week-end?")
+
+reponse = input("S 'il te plait tape : 'Ronan est trop cool!'")
+
+if reponse == "Ronan est trop cool!" :
+    print("T es cool aussi")
+else :
+    print("j aime pas ta reponse")
